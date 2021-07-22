a866c74e27e4bd3985aaa663da64ba28710e0f38
erdemcicek <erdem25123@gmail>
2021-07-22 16:15:34 +0200

    first commit

---
+++ jwtdemo2021/src/main/java/jwt21/Jwtdemo2021Application.java
@@ -0,0 +1,13 @@
+package jwt21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtdemo2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwtdemo2021Application.class, args);
+	}
+
+}
---
+++ jwtdemo2021/src/test/java/com/techproed/jwtdemo2021/Jwtdemo2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techproed.jwtdemo2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwtdemo2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
